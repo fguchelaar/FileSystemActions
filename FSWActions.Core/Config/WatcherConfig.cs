f1a05b835a152045b7b97cc55fdbaad6d1cb8c09
guchelaarf <f.guchelaar@umcg.nl>
2014-12-03 16:29:43 +0100

    Implemented basic configuration classes

---
+++ FSWActions.Core/Config/WatcherConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Xml.Serialization;
+
+namespace FSWActions.Core.Config
+{
+    public class WatcherConfig
+    {
+        [XmlElement("action")]
+        public ActionConfigCollection ActionsConfig { get; set; }
+    }
+}
---
+++ FSWActions.Core/Config/WatchersConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace FSWActions.Core.Config
+{
+    [XmlRoot("watchers")]
+    public class WatchersConfiguration
+    {
+        [XmlElement("watcher")]
+        public Collection<WatcherConfig> Watchers { get; set; }
+
+        public static WatchersConfiguration LoadConfigFromPath(string configPath)
+        {
+            FileStream fs = null;
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(WatchersConfiguration));
+                fs = new FileStream(configPath, FileMode.Open, FileAccess.Read);
+                return (WatchersConfiguration)serializer.Deserialize(fs);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
+        }
+    }
+}
