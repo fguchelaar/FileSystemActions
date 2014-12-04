200691814ac46c694a5b3fc4396fa5fc0eaa7726
Frank Guchelaar <fguchelaar@gmail.com>
2014-12-04 11:42:13 +0100

    Most simple form of starting a process implemented

--- FSWActions.ConsoleApplication/Program.cs
+++ FSWActions.ConsoleApplication/Program.cs
@@ -14,14 +14,14 @@ namespace FSWActions.ConsoleApplication
             {
                 foreach (ActionConfig action in watcherConfig.ActionsConfig)
                 {
-                    Console.WriteLine("[{0}] {1}: {2}", watcherConfig, action.Event, action.Command);
+                    Console.WriteLine("{0} [{1}]\t{2}: {3}", watcherConfig.Path, watcherConfig.Filter, action.Event, action.Command);
                 }
                 Watcher watcher = new Watcher(watcherConfig);
                 watcher.StartWatching();
             }
 
             // Wait for the user to quit the program.
-            Console.WriteLine("Press \'q\' to quit");
+            Console.WriteLine("\n\nPress \'q\' to quit\n");
             while (Console.Read() != 'q') ;
         }
     }
