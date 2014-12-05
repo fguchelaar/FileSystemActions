1b9682442be088de4425e477d70a916be685509c
Frank Guchelaar <fguchelaar@gmail.com>
2014-12-05 10:35:53 +0100

    Untested code additions to Service project :-|

--- FSWActions.WindowsService/Service.cs
+++ FSWActions.WindowsService/Service.cs
@@ -1,9 +1,14 @@
-﻿using System.ServiceProcess;
+﻿using System.Collections.Generic;
+using System.ServiceProcess;
+using FSWActions.Core;
+using FSWActions.Core.Config;
 
 namespace FSWActions.WindowsService
 {
     public partial class Service : ServiceBase
     {
+        private List<Watcher> Watchers { get; set; }
+
         public Service()
         {
             InitializeComponent();
@@ -11,10 +16,23 @@ namespace FSWActions.WindowsService
 
         protected override void OnStart(string[] args)
         {
+            WatchersConfiguration configuration = WatchersConfiguration.LoadConfigFromPath("watchers.xml");
+            Watchers = new List<Watcher>(configuration.Watchers.Count);
+
+            foreach (WatcherConfig watcherConfig in configuration.Watchers)
+            {
+                Watcher watcher = new Watcher(watcherConfig);
+                Watchers.Add(watcher);
+                watcher.StartWatching();
+            }
         }
 
         protected override void OnStop()
         {
+            foreach (Watcher watcher in Watchers)
+            {
+                watcher.StopWatching();
+            }
         }
     }
 }
