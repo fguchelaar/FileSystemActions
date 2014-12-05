d6d14009aa467e53e48deb335462f8c548c7e47c
Frank Guchelaar <fguchelaar@gmail.com>
2014-12-05 10:21:54 +0100

    Stop all watchers when exiting

--- FSWActions.ConsoleApplication/Program.cs
+++ FSWActions.ConsoleApplication/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using FSWActions.Core;
 using FSWActions.Core.Config;
 
@@ -9,6 +10,7 @@ namespace FSWActions.ConsoleApplication
         static void Main(string[] args)
         {
             WatchersConfiguration configuration = WatchersConfiguration.LoadConfigFromPath("watchers.xml");
+            List<Watcher> watchers = new List<Watcher>(configuration.Watchers.Count);
 
             foreach (WatcherConfig watcherConfig in configuration.Watchers)
             {
@@ -17,12 +19,19 @@ namespace FSWActions.ConsoleApplication
                     Console.WriteLine("{0} [{1}]\t{2}: {3}", watcherConfig.Path, watcherConfig.Filter, action.Event, action.Command);
                 }
                 Watcher watcher = new Watcher(watcherConfig);
+                watchers.Add(watcher);
                 watcher.StartWatching();
             }
 
             // Wait for the user to quit the program.
             Console.WriteLine("\n\nPress \'q\' to quit\n");
             while (Console.Read() != 'q') ;
+
+            // Stop all watchers, not really necessary though...
+            foreach (Watcher watcher in watchers)
+            {
+                watcher.StopWatching();
+            }
         }
     }
 }
